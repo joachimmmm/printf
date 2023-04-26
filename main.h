37a60a00e499e0766a699447b3da73d315ba4d6a
Eshobabei <estheronwuka96@gmail.com>
2023-04-25 19:21:34 -0700

    correction

--- main.h
+++ main.h
@@ -12,11 +12,5 @@ int _putstr(char *str);
 
 int _printf(const char *format, ...);
 
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-
 
 #endif
